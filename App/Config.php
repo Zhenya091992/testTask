7e9943c33c30f36ef4aedfb37af2ea93893a5a4a
zhenya091992 <zhenya091992@gmail.com>
2022-09-04 14:10:19 +0300

    Config class +routs

---
+++ App/Config.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Config
+ *
+ * @package App
+ */
+class Config
+{
+    /**
+     * @var null|self contain object self class
+     */
+    protected static $instance = null;
+
+    /**
+     * @var array contain array config data
+     */
+    public $configData;
+
+    public $routs;
+
+    /**
+     * create singleton Config
+     *
+     * @return Config
+     */
+    public static function instance()
+    {
+        return !static::$instance ? static::$instance = new static() : static::$instance;
+    }
+
+    /**
+     * Config protected constructor.
+     *
+     * read config.php file and fill $configData
+     */
+    protected function __construct()
+    {
+        //$this->configData = include __DIR__ . '/../config/config.php';
+        $this->routs = include __DIR__ . '/../config/routs.php';
+    }
+}
---
+++ config/routs.php
@@ -0,0 +1,5 @@
+<?php
+// ['uri' => ['Controller', 'action']]
+return [
+'/Autorisation' => ['controller' => 'App\Controllers\Controller', 'action' => 'autorisation']
+];
