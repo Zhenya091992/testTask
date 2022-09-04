78b321efbec7cc2fc906f1a3a382d5ebcb68f62a
zhenya091992 <zhenya091992@gmail.com>
2022-09-04 14:10:58 +0300

    index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'test';
+use App\Router;
+use App\Config;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$config = Config::instance();
+
+$router = new Router($config->routs);
+$router->routing($_SERVER['REQUEST_URI']);
