69474da83a3aee776ff550837d3a697eaa6501bd
yayheniy <zhenya091992@gmail.com>
2022-09-22 02:33:02 +0300

    create request

---
+++ App/Request.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+class Request
+{
+    protected $data;
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    public function setJsonData($json)
+    {
+        $std = json_decode($json);
+        foreach ($std as $property => $value) {
+            $this->data[$property] = $value;
+        }
+    }
+
+    public function getData($name)
+    {
+        return $this->data[$name];
+    }
+}
