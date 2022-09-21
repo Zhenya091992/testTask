4e64e39d4ad02c85c0d654480374af31adba7ed0
yayheniy <zhenya091992@gmail.com>
2022-09-22 02:31:44 +0300

    Access controller

--- App/Controllers/AccessController.php
+++ App/Controllers/AccessController.php
@@ -2,13 +2,34 @@
 
 namespace App\Controllers;
 
-use App\View;
+use App\Config;
+use App\JsonStorage;
+use App\Validators\Rules\RegisterRule;
+use App\Models\User;
 
 class AccessController extends Controller
 {
-    public function registration()
+    public function registration($errors)
     {
-        $this->view->display('registration/content.twig', []);
+        $this->view->display('registration/content.twig', $errors ?? []);
+    }
+
+    public function register()
+    {
+
+        $this->request->setData($_POST);
+        $rule = new RegisterRule();
+        if ($errors = $rule->validate($this->request)) {
+            echo json_encode($errors);
+        }
+
+        $user = new User();
+        $user->login = $this->request->getData('login');
+        $user->name = $this->request->getData('nameUser');
+        $user->email = $this->request->getData('email');
+        $user->password = $this->request->getData('password');
+        $user->save();
+
     }
 
     public function authentication()
