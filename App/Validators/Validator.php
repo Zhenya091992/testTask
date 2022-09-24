2e5bbab9c554a61a55aa2e8cb3419a6ed2dad419
yayheniy <zhenya091992@gmail.com>
2022-09-24 03:35:00 +0300

    add rules

--- App/Validators/Validator.php
+++ App/Validators/Validator.php
@@ -2,6 +2,7 @@
 
 namespace App\Validators;
 
+use App\JsonStorage;
 use App\Request;
 use App\Validators\Rules\RuleIntarface;
 
@@ -27,7 +28,52 @@ class Validator
     public function minLength(int $length, string $message)
     {
         if (strlen($this->value) < $length) {
-            $this->errors[$this->name][] = $message;
+            $this->errors[] = $message;
+        }
+
+        return $this;
+    }
+
+    public function unique(JsonStorage $storage, string $message)
+    {
+        if ($storage->findWere('user', [$this->name => $this->value])) {
+            $this->errors[] = $message;
+        }
+
+        return $this;
+    }
+
+    public function numbsAndLetters($message)
+    {
+        if (preg_match('@^\d\d*\pL+[\d\pL]*|\pL\pL*\d+[\d\pL]*$@SDs', $this->value)) {
+            $this->errors[] = $message;
+        }
+
+        return $this;
+    }
+
+    public function mustMatch($value, $message)
+    {
+        if ($value !== $this->value) {
+            $this->errors[] = $message;
+        }
+
+        return $this;
+    }
+
+    public function email($message)
+    {
+        if (!filter_var($this->value, FILTER_VALIDATE_EMAIL)) {
+            $this->errors[] = $message;
+        }
+
+        return $this;
+    }
+
+    public function onlyLetters($message)
+    {
+        if (!ctype_alpha($this->value)) {
+            $this->errors[] = $message;
         }
 
         return $this;
